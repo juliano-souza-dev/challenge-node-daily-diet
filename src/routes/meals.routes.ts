71987ccd31a6da34d4cf36d24b062718bde62e96
Juliano Souza <souzza.s1990@gmail.com>
2024-10-29 20:55:43 -0300

    Should be able to get the user's diet metrics

--- src/routes/meals.routes.ts
+++ src/routes/meals.routes.ts
@@ -41,9 +41,100 @@ export async function mealsRoutes(app: FastifyInstance) {
   })
   app.get('/:id', { preHandler: checkSession }, async (request, reply) => {
     const { user } = request
+    const mealIdSchema = z.object({ id: z.string().uuid() })
+    const { id } = mealIdSchema.parse(request.params)
 
-    const meal = await knex('meals').where('user_id', user?.id).first()
+    const meal = await knex('meals')
+      .where('user_id', user?.id)
+      .andWhere('id', id)
+      .first()
 
     reply.send(meal)
+  })
+  app.get('/metrics', { preHandler: checkSession }, async (request, reply) => {
+    const { user } = request
+
+    const meals = await knex('meals')
+      .where({ user_id: user?.id })
+      .orderBy('date', 'desc')
+
+    const totalMealsOnDiet = await knex('meals')
+      .count('id', { as: 'total' })
+      .where({ user_id: user?.id, is_on_diet: true })
+      .first()
+
+    const totalMealsOutsideOfTheDiet = await knex('meals')
+      .count('id', { as: 'total' })
+      .where({ user_id: user?.id, is_on_diet: false })
+      .first()
+
+    const { bestSequenceOnDiet } = meals.reduce(
+      (acc, meal) => {
+        if (meal.is_on_diet) {
+          acc.currentSequence += 1
+        } else {
+          acc.currentSequence = 0
+        }
+        if (acc.currentSequence > acc.bestSequenceOnDiet) {
+          acc.bestSequenceOnDiet = acc.currentSequence
+        }
+
+        return acc
+      },
+      {
+        bestSequenceOnDiet: 0,
+        currentSequence: 0,
+      },
+    )
+
+    reply.status(200).send({
+      mealsTotalQuantity: meals.length,
+      totalMealsOnDiet: totalMealsOnDiet?.total,
+      totalMealsOutsideOfTheDiet: totalMealsOutsideOfTheDiet?.total,
+      bestSequenceOnDiet,
+    })
+  })
+  app.put('/:id', { preHandler: checkSession }, async (request, reply) => {
+    const { user } = request
+    const mealIdSchema = z.object({ id: z.string().uuid() })
+    const { id } = mealIdSchema.parse(request.params)
+
+    const updateMealSchema = z.object({
+      name: z.string(),
+      description: z.string(),
+      date: z.coerce.date(),
+      isOnDiet: z.boolean(),
+    })
+
+    try {
+      const { name, description, date, isOnDiet } = updateMealSchema.parse(
+        request.body,
+      )
+      await knex('meals').where({ user_id: user?.id, id }).update({
+        name,
+        description,
+        date,
+        is_on_diet: isOnDiet,
+      })
+      reply.status(204).send()
+    } catch (err) {
+      reply.status(500).send({
+        message: 'Internal Server error',
+      })
+    }
+  })
+  app.delete('/:id', { preHandler: checkSession }, async (request, reply) => {
+    const { user } = request
+    const mealIdSchema = z.object({ id: z.string().uuid() })
+    const { id } = mealIdSchema.parse(request.params)
+
+    try {
+      await knex('meals').delete().where({ user_id: user?.id, id })
+      reply.status(204).send()
+    } catch (err) {
+      reply.status(500).send({
+        message: 'Internal Server error',
+      })
+    }
   })
 }
--- test/meals.spec.ts
+++ test/meals.spec.ts
@@ -97,4 +97,112 @@ describe('users suit test', () => {
       .expect(200)
     expect(responseMeal.body).toHaveProperty('id')
   })
+  it('should be able to delete a specific meal', async () => {
+    const userResponse = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@gmail.com',
+      password: '123',
+    })
+
+    const cookie = userResponse.get('Set-Cookie')
+    await request(app.server)
+      .post('/meals')
+      .set('Cookie', cookie!)
+      .send({
+        name: 'dinner',
+        description: 'last meal',
+        date: new Date().setHours(18, 0, 0),
+        isOnDiet: true,
+      })
+
+    const response = await request(app.server)
+      .get('/meals')
+      .set('Cookie', cookie!)
+
+    const { id } = response.body[0]
+
+    await request(app.server)
+      .delete(`/meals/${id}`)
+      .set('Cookie', cookie!)
+      .expect(204)
+  })
+  it('should be able to edit a specific meal', async () => {
+    const userResponse = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@gmail.com',
+      password: '123',
+    })
+
+    const cookie = userResponse.get('Set-Cookie')
+    await request(app.server)
+      .post('/meals')
+      .set('Cookie', cookie!)
+      .send({
+        name: 'dinner',
+        description: 'last meal',
+        date: new Date().setHours(18, 0, 0),
+        isOnDiet: true,
+      })
+
+    const response = await request(app.server)
+      .get('/meals')
+      .set('Cookie', cookie!)
+
+    const { id } = response.body[0]
+
+    await request(app.server)
+      .put(`/meals/${id}`)
+      .set('Cookie', cookie!)
+      .send({
+        name: 'dinner',
+        description: 'last meal',
+        date: new Date().setHours(18, 0, 0),
+        isOnDiet: true,
+      })
+      .expect(204)
+  })
+
+  it('Should be able to get the metrics about the users meal', async () => {
+    const userResponse = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@gmail.com',
+      password: '123',
+    })
+
+    const cookie = userResponse.get('Set-Cookie')
+
+    const meal = {
+      name: 'Breakfast',
+      description: 'First meal',
+      date: new Date(),
+      isOnDiet: true,
+    }
+    const meal2 = {
+      name: 'dinner',
+      description: 'last meal',
+      date: new Date().setHours(18, 0, 0),
+      isOnDiet: true,
+    }
+    const meal3 = {
+      name: 'other',
+      description: 'other meal',
+      date: new Date().setHours(18, 0, 0),
+      isOnDiet: false,
+    }
+
+    await request(app.server).post('/meals').set('Cookie', cookie!).send(meal)
+    await request(app.server).post('/meals').set('Cookie', cookie!).send(meal2)
+    await request(app.server).post('/meals').set('Cookie', cookie!).send(meal3)
+
+    const result = await request(app.server)
+      .get('/meals/metrics')
+      .set('Cookie', cookie!)
+
+    expect(result.body).toEqual({
+      mealsTotalQuantity: 3,
+      totalMealsOnDiet: 2,
+      totalMealsOutsideOfTheDiet: 1,
+      bestSequenceOnDiet: 2,
+    })
+  })
 })
